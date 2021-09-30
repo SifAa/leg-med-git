4ce4acb6b43fd2a9e8b5f62ed9df7b01fc5ffbd0
Sif <sif_aagaard_svendsen@hotmail.com>
2021-09-30 09:35:26 +0200

    main.js created

---
+++ main.js
@@ -0,0 +1,40 @@
+/*
+Validation requirements
+
+name:
+it letters not numbers
+over 2 characters
+not symbols
+
+lastName:
+it letters not numbers
+at least 2 characters
+not symbols
+
+phone: 
+Only numbers
+(danish) 8 numbers
+
+email:
+text to begin with
+a dot after @ symbol
+text between @ and dot
+and som text after the dot
+
+password:
+lower and uppercase letters
+at least one number
+at least 8 characters
+a symbol
+
+repeatPassword:
+should be exactly the same as password
+
+Adress:
+at least one number
+at least 3 characters
+
+zipCode:
+4 numbers (danish)
+Maybe we could validate with a array of zipcCodes.
+*/
