5d40a53bcb6deae043b5271a2315894a7583867d
Sif <sif_aagaard_svendsen@hotmail.com>
2021-09-30 09:36:46 +0200

    Validate names, phone, mail and repeat password

--- main.js
+++ main.js
@@ -38,3 +38,46 @@ zipCode:
 4 numbers (danish)
 Maybe we could validate with a array of zipcCodes.
 */
+// definer formen så man kan hente alle dataerne
+let form = document.querySelector("form");
+// definer alle regex så de kan kaldes
+let regexName = /^[a-zæøåA-ZÆØÅ\s\-]{2,}$/;
+let regexPhone = /^[\d]{8,8}$/;
+let regexMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+// let regexPass = / /;
+
+// valider fornavn
+if(regexName.test(form.name.value)){
+    console.log("only contains letters spaces and or dashes")
+}else{
+    console.log("must not contain numbers or symbols")
+}
+
+// valider efternavn
+
+if(regexName.test(form.lastName.value)){
+    console.log("only contains letters spaces and or dashes")
+}else{
+    console.log("must not contain numbers or symbols")
+}
+
+// valider telefon nr
+if(regexPhone.test(form.phone.value)){
+    console.log("only contains numbers and is exactly 8 char long")
+}else{
+    console.log("must not contain letters or symbols and must be exactly 8 char long")
+}
+
+// valider email
+if(regexMail.test(form.mail.value)){
+    console.log("has letters, underline, dashes and/or numbers in front of a @, letters between @ and a dot, as well as 2-3 char")
+}else{
+    console.log("must not contain letters or symbols and must be exactly 8 char long")
+}
+
+// valider gentagelse af password
+if(form.password.value == form.repeatPassword.value){
+    console.log("Passwords match !!!!")
+}else{
+    console.log("Passwords do not match")
+}
